99b3c98374e2b827d9f17a19170fe0729d1878f6
Amy-Jassal <amy-jassal@hotmail.co.uk>
2023-09-29 16:06:05 +0100

    Return a list of prime numbers

--- primes.py
+++ primes.py
@@ -2,5 +2,17 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if(number_of_primes < 1):
+        raise ValueError
     list = []
+    a = 2
+    list.append(a)
+    while(len(list) < number_of_primes):
+        a+=1
+        isPrime = True
+        for num in range(2,a):
+            if(a % num == 0):
+                isPrime = False
+        if(isPrime):
+            list.append(a)
     return list
